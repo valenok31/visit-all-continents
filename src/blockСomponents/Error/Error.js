10a593b7ea06b35d5a2178da06a37e6be59a685a
Andrey <priv1984@yandex.ru>
2022-04-15 22:03:12 +0700

    add Error

--- src/blockСomponents/Error/Error.js
+++ src/blockСomponents/Error/Error.js
@@ -1,12 +1,30 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import style from "../home/home.module.css";
 
 const Error = (props) => {
     return <div>
         Error 404!
-        <NavLink to={'/'}>
+        <div>
+        <NavLink to={'/'} className={style.nLink}>
         <div>Mein</div>
         </NavLink>
+        </div>
+        <div>
+            <NavLink to='/game' className={style.nLink}>
+                <div>Start</div>
+            </NavLink>
+        </div>
+        <div>
+            <NavLink to='/gamerules' className={style.nLink}>
+                <div>Game rules</div>
+            </NavLink>
+        </div>
+        <div>
+            <NavLink to='/records' className={style.nLink}>
+                <div>Records</div>
+            </NavLink>
+        </div>
     </div>
 }
 
