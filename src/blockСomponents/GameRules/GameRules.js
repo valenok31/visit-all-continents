e33e28fdbea3fc6009553c95a443347ef21a0c72
Andrey <priv1984@yandex.ru>
2022-04-27 01:20:13 +0700

    Preparation for build

--- src/blockСomponents/GameRules/GameRules.js
+++ src/blockСomponents/GameRules/GameRules.js
@@ -17,16 +17,6 @@ const GameRules = (props) => {
             start from any city. The city can be visited only once. It is not necessary to visit all cities, the main
             thing is to visit all six continents. Good luck!
         </div>
-        <div className={style.game_rules}>Die Regeln des Spiels sind sehr einfach. Sie müssen alle Kontinente mit der
-            billigsten Route besuchen. Sie
-            können von jeder Stadt aus starten. Die Stadt kann nur einmal besucht werden. Es ist nicht notwendig, alle
-            Städte zu besuchen, die Hauptsache ist, alle sechs Kontinente zu besuchen. Viel Glück!
-        </div>
-        <div className={style.game_rules}>Las reglas del juego son muy simples. Es necesario visitar todos los
-            continentes por la ruta más barata.
-            Puedes comenzar desde cualquier ciudad. La ciudad solo se puede visitar una vez. No es necesario visitar
-            todas las ciudades, lo principal es visitar los seis continentes. ¡Mucho éxito!
-        </div>
     </>
 }
 
