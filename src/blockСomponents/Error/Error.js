94f35ae70439b0421de7ab7ba0e301bd98f40614
Andrey <priv1984@yandex.ru>
2022-05-01 23:04:08 +0700

    Error css

--- src/blockСomponents/Error/Error.js
+++ src/blockСomponents/Error/Error.js
@@ -1,29 +1,31 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-import style from "../home/home.module.css";
+import style from "./Error.module.css";
 
 const Error = (props) => {
     return <div className={style.main}>
-        Error 404!
-        <div>
-        <NavLink to={'/'} className={style.nLink}>
-        <div>Mein</div>
-        </NavLink>
-        </div>
-        <div>
-            <NavLink to='/game' className={style.nLink}>
-                <div>Start</div>
-            </NavLink>
-        </div>
-        <div>
-            <NavLink to='/gamerules' className={style.nLink}>
-                <div>Game rules</div>
-            </NavLink>
-        </div>
-        <div>
-            <NavLink to='/records' className={style.nLink}>
-                <div>Records</div>
-            </NavLink>
+        <div className={style.block}>
+            Error 404!
+            <div>
+                <NavLink to={'/'} className={style.nLink}>
+                    <div>Mein</div>
+                </NavLink>
+            </div>
+            <div>
+                <NavLink to='/game' className={style.nLink}>
+                    <div>Start</div>
+                </NavLink>
+            </div>
+            <div>
+                <NavLink to='/gamerules' className={style.nLink}>
+                    <div>Game rules</div>
+                </NavLink>
+            </div>
+            <div>
+                <NavLink to='/records' className={style.nLink}>
+                    <div>Records</div>
+                </NavLink>
+            </div>
         </div>
     </div>
 }
